7159a8b4769860254f5825d6563948d53ebc5794
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2025-06-11 17:23:37 +0200

    Initial commit - Opsætning af backend filstrucktur

---
+++ src/main/java/rc/backendeksamen/BackendEksamenApplication.java
@@ -0,0 +1,13 @@
+package rc.backendeksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEksamenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rc/backendeksamen/BackendEksamenApplicationTests.java
@@ -0,0 +1,13 @@
+package rc.backendeksamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEksamenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
